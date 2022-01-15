1a280b98240f62c532ac55c4028655c040c123d9
kylejames20 <k.salmons@me.com>
2022-01-15 11:50:44 -0700

    Final updates to JS, README and added links along with screenshot

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # JavaScript-PasswordGen
+
+Password Generator Homework
+
+This repository lets user select what type of password they would like.
+
+1. Allows user to generate a password.
+2. Prompts user to select how many characters they want to be in their password.
+3. Prompts user to decide if they want lower case letters, upper case letters, numbers and/or special characters.
+4. Once all prompts have been answered, site will generate randomized password for user.
+
+Below, find attached links to Github repository and deployed finished project.
+
+![Password Generator site screen shot](./PWGenerator.png)
+
+[Live link to site](https://kylejames20.github.io/JavaScript-PasswordGen/)
+
+[Direct link to Github repository](https://github.com/kylejames20/JavaScript-PasswordGen)
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Assignment Code
+//assigning varibles to work throughout the JavaScript
 var generateBtn = document.querySelector("#generate");
 var passwordLength = 8;
 var lowerCase = true;
@@ -9,16 +9,14 @@ const symbols = ["!","#","$",".","%","&","(",")","*","+","-",".","/",":",";","<"
 const lowerAbc = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 const upperAbc = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 const numeros = ["0","1","2","3","4","5","6","7","8","9"];
-
-
-// Write password to the #password input
+//first function, letting user start the password process
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   
   passwordText.value = password; 
-  
-}
+  }
+  //making sure the user maintains a password between designated character parameters
 function passwordLengthCheck() {
     let passwordSize = prompt("How many characters do you want?")
      if (passwordSize < 8 || passwordSize > 128){
@@ -26,33 +24,32 @@ function passwordLengthCheck() {
     passwordLengthCheck()
   } else {return passwordSize}
 }
+//once user selects character amount, this asks user what character styles they want to use
 function generatePassword() {
   var passString = "";
   var passwordItems = [];
-
   let passwordLength = passwordLengthCheck()
-  //we will need an if statement here. if the user types in less than 8/more than 128, re prompt user
-  // passwordLength = window.prompt("How many characters do you want?");
-  // if (passwordLength < 8 || passwordLength > 128){
-  //   alert("Your password must be between 8 - 128 characters.")
-  //   generatePassword()
-  // }
   lowerCase = window.confirm ("Do you want lower case letters?");
   upperCase = window.confirm ("Do you want upper case letters?");
   numbers = window.confirm("Do you wany numbers?");
   specialChar = window.confirm("Do you want special characters?");
-if (lowerCase == true){
+//asking user if they want lower case letters
+  if (lowerCase == true){
     passwordItems = passwordItems.concat(lowerAbc);
   }
+  //asking user if they want upper case letters
   if(upperCase == true){
     passwordItems = passwordItems.concat(upperAbc);
   }
+  //asking user if they want numbers
   if(numbers == true){
     passwordItems = passwordItems.concat(numeros);
   }
+  //asking user if they want special characters
   if(specialChar == true){
     passwordItems = passwordItems.concat(symbols);
   }
+  //allows me to check console log if problems arise
   console.log(passwordItems);
   console.log(passwordItems[14]);
   
